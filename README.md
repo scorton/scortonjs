9f986842dd5e7a940c1bf803007af23e3d5b65fc
Bacely YoroBi <yorogoule@gmail.com>
2025-06-25 03:10:07 +0200

    Add placeholders for new commands

--- README.md
+++ README.md
@@ -12,5 +12,8 @@ Available commands:
 
 - `hello`    prints a greeting message
 - `version`  prints the package version
+- `init`     initializes a project *(not implemented yet)*
+- `generate` scaffolds code *(not implemented yet)*
+- `build`    builds project artifacts *(not implemented yet)*
 
 Without a command, help is shown.
--- cli.js
+++ cli.js
@@ -12,19 +12,27 @@ function showHelp() {
   console.log('Commands:');
   console.log('  hello    Print greeting');
   console.log('  version  Print version');
+  console.log('  init     Initialize project (not implemented)');
+  console.log('  generate Generate code (not implemented)');
+  console.log('  build    Build project (not implemented)');
 }
 
-switch (command) {
-  case 'hello':
-    console.log('Hello from scortonjs!');
-    break;
-  case 'version':
-    const pkg = JSON.parse(
-      readFileSync(join(__dirname, 'package.json'), 'utf8')
-    );
-    console.log(pkg.version);
-    break;
-  default:
-    showHelp();
-    break;
-}
+  switch (command) {
+    case 'hello':
+      console.log('Hello from scortonjs!');
+      break;
+    case 'version':
+      const pkg = JSON.parse(
+        readFileSync(join(__dirname, 'package.json'), 'utf8')
+      );
+      console.log(pkg.version);
+      break;
+    case 'init':
+    case 'generate':
+    case 'build':
+      console.log('Not implemented yet.');
+      break;
+    default:
+      showHelp();
+      break;
+  }
